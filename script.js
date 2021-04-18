f652f5579f0d9198543eaed7607402c502f9ea46
Tsvylenyeva <82768011+Tsvylenyeva@users.noreply.github.com>
2021-04-18 18:12:25 +0300

    Add files via upload
    loading files

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let emailButton=document.querySelector('.emailButton');
+emailButton.onclick=function () {
+    let input = document.querySelector('input');
+    message textContent = input.value + ", дякуємо за підписку!"
+}
